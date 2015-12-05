8cfb50619a8bd3f6a664e63947f485768ae5fb9d
sdcuike <303286730@qq.com>
2015-12-05 16:21:40 +0800

    Additional Thread Capabilities

---
+++ Java-Threads-And-The-ConcurrencyUtilities/src/main/java/com/doctor/ch04/Sec01.java
@@ -0,0 +1,20 @@
+package com.doctor.ch04;
+
+/**
+ * @author sdcuike
+ *
+ * @time 2015年12月5日下午2:59:54
+ * 
+ *       Each ThreadLocal instance describes a thread-local variable, which is a variable that
+ *       provides a separate storage slot to each thread that accesses the variable. You can think of
+ *       a thread-local variable as a multislot variable in which each thread can store a different
+ *       value in the same variable. Each thread sees only its value and is unaware of other threads
+ *       having their own values in this variable.
+ */
+public class Sec01 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
