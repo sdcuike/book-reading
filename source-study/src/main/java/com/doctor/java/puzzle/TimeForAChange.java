160d016ba88692126c2a2ac6309b5bc0c919f57a
sdcuike <303286730@qq.com>
2016-01-31 21:48:04 +0800

    update

--- source-study/src/main/java/com/doctor/java/puzzle/TimeForAChange.java
+++ source-study/src/main/java/com/doctor/java/puzzle/TimeForAChange.java
@@ -9,9 +9,16 @@ import java.math.BigDecimal;
  * 
  *       java Puzzles 之关于金钱计算那些事，java要用int、long、or BigDecimal来计算，一般我们BigDecimal。
  * 
+ *       警告：用BigDecimal(String)这个构造函数，而非 BigDecimal(double)。
+ * 
  * 
  *       In summary, avoid float and double where exact answers are required;
  *       for monetary calculations, use int, long, or BigDecimal。
+ *       not all decimals can be represented exactly using binary floating-point.
+ * 
+ *       There is one caveat: Always use the BigDecimal(String) constructor,
+ *       never BigDecimal(double). The latter constructor creates an instance with the
+ *       exact value of its argument: new BigDecimal(.1) returns a BigDecimal representing 0.1000000000000000055511151231257827021181583404541015625.
  * 
  */
 public class TimeForAChange {
@@ -22,6 +29,10 @@ public class TimeForAChange {
         System.out.println(2.00D - 1.10D);// 0.8999999999999999
 
         System.out.println(2.00F - 1.10F);// 0.9
+        System.out.println(Double.toString(2.00D));// 2.0
+
+        System.out.println(BigDecimal.valueOf(2.00).subtract(BigDecimal.valueOf(1.10)));// 0.9
+        System.out.println(BigDecimal.valueOf(2.00F).subtract(BigDecimal.valueOf(1.10F)));// 0.899999976158142
 
         System.out.println(BigDecimal.valueOf(2.00D).subtract(BigDecimal.valueOf(1.10D)));// 0.9
 
